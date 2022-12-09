27b7fe2f0a444ee7b1ed6a691ba0177d3f091d64
sakaamotors <sakaamotors@gmail.com>
2022-12-10 03:41:47 +0500

    init

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -153,7 +153,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 // @route   GET /api/products/top
 // @access  Public
 const getTopProducts = asyncHandler(async (req, res) => {
-  const limit = 3
+  const limit = 4
   const products = await Product.find({}).sort({ rating: -1 }).limit(limit)
 
   res.json(products)
