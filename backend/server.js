6873035c3571f42d2acc85f78e87d7228b6c5b71
sakaamotors <sakaamotors@gmail.com>
2022-12-22 10:11:04 +0500

    init

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,9 @@ const app = express()
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.errorHandler());
+}
 app.use(cors())
 
 
